a0c5d24575f372a89c3ad0c31a530369f95e4c77
nghiavohuynhdai <nghiavo.developer@gmail.com>
2024-11-13 16:43:18 +0700

    [FEAT][NghiaVHD] view course combo list, detail and delete course combo

---
+++ src/pages/course-combo/detail/components/child-course/child-course-columns.tsx
@@ -0,0 +1,93 @@
+import { Typography } from '@mui/material'
+import { MRT_ColumnDef } from 'material-react-table'
+import { ChildCourseDetailDto } from '~/data/course-combo/courseCombo'
+import { CourseLevel } from '~/global/constants'
+import { formatCourseLevel, formatCurrency } from '~/utils/format'
+
+export const childCourseColumns: MRT_ColumnDef<ChildCourseDetailDto>[] = [
+  {
+    accessorKey: 'code',
+    header: 'Mã khóa học',
+    size: 150,
+    grow: false
+  },
+  {
+    accessorKey: 'title',
+    header: 'Tên khóa học'
+  },
+  {
+    accessorKey: 'price',
+    header: 'Giá',
+    size: 100,
+    grow: false,
+    muiTableHeadCellProps: {
+      align: 'right'
+    },
+    muiTableBodyCellProps: {
+      align: 'right'
+    },
+    Cell: ({ cell }) => {
+      const price = cell.getValue() as number
+      return formatCurrency(price)
+    },
+    enableColumnFilter: false
+  },
+  {
+    accessorKey: 'level',
+    header: 'Cấp độ',
+    size: 100,
+    grow: false,
+    Cell: ({ cell }) => {
+      const level = cell.getValue() as CourseLevel
+
+      return (
+        <Typography
+          variant='subtitle2'
+          color={
+            level === CourseLevel.BASIC
+              ? '#20c017'
+              : level === CourseLevel.INTERMEDIATE
+                ? '#ffcf22'
+                : level === CourseLevel.ADVANCED
+                  ? '#f66868'
+                  : undefined
+          }
+        >
+          {formatCourseLevel(level)}
+        </Typography>
+      )
+    },
+    filterVariant: 'multi-select',
+    filterSelectOptions: [
+      { label: 'Cơ bản', value: CourseLevel.BASIC },
+      { label: 'Trung bình', value: CourseLevel.INTERMEDIATE },
+      { label: 'Nâng cao', value: CourseLevel.ADVANCED }
+    ],
+    enableSorting: false
+  },
+  {
+    id: 'type',
+    accessorKey: 'type',
+    header: 'Thể loại',
+    size: 120,
+    grow: false,
+    filterVariant: 'select',
+    filterSelectOptions: [],
+    Cell: ({ cell }) => {
+      return (cell.getValue() as []).join(', ')
+    },
+    enableSorting: false
+  },
+  {
+    accessorKey: 'learnerLimit',
+    header: 'Giới hạn học viên',
+    size: 170,
+    grow: false,
+    muiTableHeadCellProps: {
+      align: 'right'
+    },
+    muiTableBodyCellProps: {
+      align: 'right'
+    }
+  }
+]
---
+++ src/pages/course-combo/list/ViewCourseComboList.tsx
@@ -0,0 +1,109 @@
+import { MRT_ColumnFiltersState, MRT_PaginationState, MRT_SortingState } from 'material-react-table'
+import { useEffect, useState } from 'react'
+import PageHeader from '~/components/header/PageHeader'
+import Table from '~/components/table/Table'
+import { ListResponseDto } from '~/data/common.dto'
+import { ErrorResponseDto } from '~/data/error.dto'
+import { protectedRoute } from '~/routes/routes'
+import { notifyError } from '~/utils/toastify'
+import { CourseComboColumns } from './columns'
+import { Link, useNavigate } from 'react-router-dom'
+import { useCourseComboApi } from '~/hooks/api/useCourseComboApi'
+import { CourseComboListItemResponseDto } from '~/data/course-combo/courseCombo'
+import { Add } from '@mui/icons-material'
+import { Box, Button } from '@mui/material'
+
+export default function ViewCourseComboList() {
+  const [data, setData] = useState<ListResponseDto<CourseComboListItemResponseDto>>({
+    docs: [],
+    totalDocs: 0,
+    offset: 0,
+    limit: 0,
+    totalPages: 0,
+    page: 0,
+    pagingCounter: 0,
+    hasPrevPage: false,
+    hasNextPage: false,
+    prevPage: null,
+    nextPage: null
+  })
+  const [error, setError] = useState<ErrorResponseDto | null>(null)
+  const navigate = useNavigate()
+  const { getCourseComboList } = useCourseComboApi()
+  const [pagination, setPagination] = useState<MRT_PaginationState>({
+    pageIndex: 0,
+    pageSize: 10
+  })
+  const [sorting, setSorting] = useState<MRT_SortingState>([])
+  const [columnFilters, setColumnFilters] = useState<MRT_ColumnFiltersState>([])
+
+  useEffect(() => {
+    ;(async () => {
+      const { data: courseComboList, error: apiError } = await getCourseComboList(
+        pagination.pageIndex + 1,
+        pagination.pageSize,
+        sorting.map((sort) => ({ field: sort.id, desc: sort.desc })),
+        columnFilters.map((filter) => ({ field: filter.id, value: filter.value }))
+      )
+      if (courseComboList) {
+        setData(courseComboList)
+      } else {
+        setData({
+          docs: [],
+          totalDocs: 0,
+          offset: 0,
+          limit: 0,
+          totalPages: 0,
+          page: 0,
+          pagingCounter: 0,
+          hasPrevPage: false,
+          hasNextPage: false,
+          prevPage: null,
+          nextPage: null
+        })
+      }
+      setError(apiError)
+    })()
+  }, [getCourseComboList, pagination.pageIndex, pagination.pageSize, sorting, columnFilters])
+
+  if (error) {
+    notifyError(error.message)
+  }
+
+  return (
+    <>
+      <Box display='flex' justifyContent='space-between' alignItems='center'>
+        <PageHeader title='Combo khóa học' />
+        <Button color='secondary' component={Link} to={protectedRoute.createCourse.path} endIcon={<Add />}>
+          Tạo Combo
+        </Button>
+      </Box>
+      <Table
+        title='Danh sách Combo khóa học'
+        tableOptions={{
+          columns: CourseComboColumns,
+          data: data.docs || [],
+          rowCount: data.totalDocs,
+          layoutMode: 'grid',
+          onPaginationChange: setPagination,
+          onSortingChange: setSorting,
+          onColumnFiltersChange: setColumnFilters,
+          muiTableBodyRowProps: ({ row }) => ({
+            onClick: () => navigate(protectedRoute.courseComboDetail.path.replace(':id', row.original._id)),
+            sx: {
+              cursor: 'pointer'
+            }
+          }),
+          muiPaginationProps: {
+            rowsPerPageOptions: [5, 10, 20]
+          },
+          state: {
+            pagination: pagination,
+            sorting: sorting,
+            columnFilters: columnFilters
+          }
+        }}
+      />
+    </>
+  )
+}
